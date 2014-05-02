86ba7586053a8da117ba934057e15918a7e7e147
Khimya <khimyakhetarpal@gmail.com>
2014-05-02 12:52:51 +0530

    Search a number in an array of length n

---
+++ elementsearch.py
@@ -0,0 +1,17 @@
+#search for an element in an array 
+
+
+def search(element,array):
+    n = len(array)
+    for i in range(0,n):
+        if (X[i] == element):
+            return 1
+    return 0
+
+
+
+X = [8,7,6,5,77,10,1,36,4,0,11,15,3]
+number = int(raw_input("Enter the element you want to search"))
+t = search(number,X)
+if t == 1:
+    print "Found a Match"
